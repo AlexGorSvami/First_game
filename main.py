4d3db0b3706e1eb53072bd76855c82f253dbdab3
AlexGorSvami <jagger15696@gmail.com>
2023-07-12 16:45:54 +0300

    Add game icon and screen.fill

--- main.py
+++ main.py
@@ -2,5 +2,23 @@ import pygame
 
 
 pygame.init()
-screen = pygame.display.set_mode((600, 300))
+screen = pygame.display.set_mode((600, 300), )  #flags=pygame.NOFRAME
+pygame.display.set_caption('My first game')
+icon = pygame.image.load('images/game_icon.png')
+pygame.display.set_icon(icon)
 
+running = True
+
+while running:
+
+    # screen.fill((50, 168, 98))
+
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_p:
+                screen.fill((50, 121, 168))
