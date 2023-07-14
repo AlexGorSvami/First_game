c6f85b13c0a556e85c87a0b4a7a09be3f7a2ebb1
AlexGorSvami <jagger15696@gmail.com>
2023-07-14 16:38:12 +0300

    Train added fonts, icons and figure

--- main.py
+++ main.py
@@ -7,18 +7,28 @@ pygame.display.set_caption('My first game')
 icon = pygame.image.load('images/game_icon.png')
 pygame.display.set_icon(icon)
 
+
+
+square = pygame.Surface((50, 150))
+square.fill('Green')
+
+myfont = pygame.font.Font('fonts/Tektur-ExtraBold.ttf', 40)
+text_surface = myfont.render('First game', True, 'Blue')
+
+player = pygame.image.load('images/game_icon.png')
+
+
 running = True
 
 while running:
 
-    # screen.fill((50, 168, 98))
-
+    pygame.draw.circle(screen, 'Blue', (10, 20), 5)
+    screen.blit(square, (10, 0))
+    screen.blit(text_surface, (300, 100))
+    screen.blit(player, (50, 50))
     pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_p:
-                screen.fill((50, 121, 168))
